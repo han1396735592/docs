8a71e11d6556829a0811330af8dd5daf363e15a8
han1396735592 <1396735592@qq.com>
2019-04-13 02:07:04 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ vuepress build
 # 导航到构建输出目录
 mv .vuepress/dist .
 mv dist docs
-echo 'test.github.qqhxj.cn' > docs/CNAME
+echo 'docs.github.qqhxj.cn' > docs/CNAME
 git init
 git add .
 git commit -m 'deploy'
 
 # 推到你仓库的的 gh-page 分支
 # 将 <USERNAME>/<REPO> 替换为你的信息
-git push -f git@github.com:/han1396735592/test.git master
+git push -f git@github.com:/han1396735592/docs.git master
