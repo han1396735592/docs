eab0cc0f856b99d0dbe8db696d0e6fc37c804a14
韩新建 <1396735592@qq.com>
2020-02-24 19:23:40 +0800

    02/24/2020 19:23:39

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-# rm -rf docs
+rm -rf docs
 
 # vuepress build 
-./deploy-oss.sh
+# ./deploy-oss.sh
 # 导航到构建输出目录
 # mv .vuepress/dist .
 # mv dist docs
-echo 'docs.github.qqhxj.cn' > docs/CNAME
-git init
+# echo 'docs.github.qqhxj.cn' > docs/CNAME
+# git init
 git add .
-git commit -m 'deploy'
+git commit -m 'deploy on '$(date) 
 
 # 推到你仓库的的 gh-page 分支
 # 将 <USERNAME>/<REPO> 替换为你的信息
